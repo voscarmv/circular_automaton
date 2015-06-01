2b50a28721e8a9e9fa98e19d119eac74157ab810
Vicente Oscar Mier Vela <vomv1988@gmail.com>
2015-06-01 10:48:24 -0500

    Video annotations in spanish

--- video.sh
+++ video.sh
@@ -27,10 +27,10 @@ while read FNAME CELLSAREA CIRCLEAREA CELLSTOCIRCLE HITS HITSTOCIRCLE; do
 	ITER=`echo $FNAME | sed 's/cell//;s/.ppm//'`
 
 	if test $CIRCLEAREA -ge $CELLSAREA ; then
-		ANNOTATION="Iteration: $ITER\nPixels in circle: $CIRCLEAREA\nPixels in culture: $CELLSAREA\nCulture pixels inside circle: $HITS\nPercentage of circle filled: $HITSTOCIRCLE\%\nCulture area is $CELLSTOCIRCLE\% the area of the circle\n\n"
+		ANNOTATION="Iteracion: $ITER\nPixels del disco: $CIRCLEAREA\nPixels del cultivo: $CELLSAREA\nPixels del cultivo en disco: $HITS\nPorciento lleno del disco: $HITSTOCIRCLE\%\nArea del cultivo es $CELLSTOCIRCLE\% el area del disco\n\n"
 		FILL='#00ff00'
 	else
-		ANNOTATION="CULTURE AREA > CIRCLE AREA\nIteration: $ITER\nPixels in circle: $CIRCLEAREA\nPixels in culture: $CELLSAREA\nCulture pixels inside circle: $HITS\nPercentage of circle filled: $HITSTOCIRCLE\%\nCulture area is $CELLSTOCIRCLE\% the area of the circle\n\n"
+		ANNOTATION="CULTIVO > DISCO\nIteracion: $ITER\nPixels del disco: $CIRCLEAREA\nPixels del cultivo: $CELLSAREA\nPixels del cultivo en disco: $HITS\nPorciento lleno del disco: $HITSTOCIRCLE\%\nArea del cultivo es $CELLSTOCIRCLE\% el area del disco\n\n"
 	fi
 
 	FILE=`echo $FNAME | sed 's/.ppm//'`
